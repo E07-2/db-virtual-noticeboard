f0416a77a765580719a6dd9613797b5c0475fd98
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2022-04-07 13:39:28 +0200

    changes

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path'
+
+const app = express();
+const port = 3001;
+const clientUrl = "http://localhost:3001";
+
+// !! Your middleware should not go below this line !!
+// Serve frontend client/build folder
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`The server 🙈 is listening on port ${port}`);
+  console.log(`Visit ${clientUrl} in your browser`);
+});
